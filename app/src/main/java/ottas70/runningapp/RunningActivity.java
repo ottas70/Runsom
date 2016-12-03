53a144b5aaeb455b4971ceb5f39a873d2778a74f
Otto Vodvářka <ottas70@gmail.com>
2016-12-03 20:55:23 +0100

    Update

--- app/src/main/java/ottas70/runningapp/RunningActivity.java
+++ app/src/main/java/ottas70/runningapp/RunningActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -14,6 +15,11 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class RunningActivity extends Activity {
 
@@ -31,6 +37,7 @@ public class RunningActivity extends Activity {
     private DistanceTracker distanceTracker;
     private Handler handler = new Handler();
     private Timer timer;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +50,7 @@ public class RunningActivity extends Activity {
         startButton = (FloatingActionButton) findViewById(R.id.startButton);
 
         isRunning = false;
-        distanceTracker = new DistanceTracker(this, distanceTextView,speedTextView);
+        distanceTracker = new DistanceTracker(this, distanceTextView, speedTextView);
         timer = new Timer(timerTextView, handler, 0);
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -58,13 +65,14 @@ public class RunningActivity extends Activity {
             }
         });
 
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void startRun() {
         isRunning = true;
 
-        startButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(String.valueOf(R.color.stopColor))));
-        startButton.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_stop_black_36dp));
+        startButton.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+        startButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_stop_black_36dp));
 
         handler.postDelayed(timer, 1000);
         distanceTracker.startLocationUpdates();
@@ -75,8 +83,8 @@ public class RunningActivity extends Activity {
     private void stopRun() {
         isRunning = false;
 
-        startButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(String.valueOf(R.color.startColor))));
-        startButton.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_play_arrow_black_36dp));
+        startButton.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        startButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_play_arrow_black_36dp));
 
         handler.removeCallbacks(timer);
         distanceTracker.stopLocationUpdates();
@@ -103,4 +111,39 @@ public class RunningActivity extends Activity {
 
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Running Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
